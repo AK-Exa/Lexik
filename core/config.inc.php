5f45437ca9491ac653fd3a6089eab82d08163284
Hmidou <akrichel@yahoo.fr>
2017-11-27 21:41:09 +0100

    Premier commit

---
+++ core/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database
+ */
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'lexik');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+/**
+ * Redbean configuration
+ */
+define('REDBEAN_MODEL_PREFIX', '\\Lexik\\Model\\');
